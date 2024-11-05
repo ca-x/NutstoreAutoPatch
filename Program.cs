97b8e0ead32c567e2d0c01e19146fe925ae95b06
czyt <czyt@w.cn>
2024-11-05 22:51:58 +0800

    adjust code

--- Program.cs
+++ Program.cs
@@ -9,64 +9,70 @@ namespace NutstoreAutoPatch
     {
         public static void Main(string[] args)
         {
-           const string nutstoreLib = "NutstoreLib.dll"; 
-           const string nutstoreLibBackup = "NutstoreLib.origin.dll"; 
-           const string directoryutils = "NutstoreLib.Utils.DirectoryUtils"; 
-           const string appdataNutstoreDir = "get_APPDATA_NUTSTORE_DIR"; 
-           
-           // backup origin 
-           File.Copy(nutstoreLib, nutstoreLibBackup, true);
-            // Read the assembly
-            var assemblyDefinition = AssemblyDefinition.ReadAssembly(nutstoreLib);
-            var directoryUtil = assemblyDefinition.MainModule.Types.First(t => t.FullName == directoryutils);
-            var appdataDirMethod = directoryUtil.Methods.First(m => m.Name == appdataNutstoreDir);
+            const string nutstoreLib = "NutstoreLib.dll";
+            const string nutstoreLibBackup = "NutstoreLib.origin.dll";
+            const string nutstoreLibTarget = "NutstoreLib.target.dll";
             
-            // 清除当前指令
-            appdataDirMethod.Body.Instructions.Clear();
+            const string directoryutils = "NutstoreLib.Utils.DirectoryUtils";
+            const string appdataNutstoreDir = "get_APPDATA_NUTSTORE_DIR";
 
-            // 获取IL处理器
-            var ilProcessor = appdataDirMethod.Body.GetILProcessor();
+            // backup origin 
+            File.Copy(nutstoreLib, nutstoreLibBackup, true);
+            // Read the assembly
+            using (var assemblyDefinition = AssemblyDefinition.ReadAssembly(nutstoreLib))
+            {
+                var directoryUtil = assemblyDefinition.MainModule.Types.First(t => t.FullName == directoryutils);
+                var appdataDirMethod = directoryUtil.Methods.First(m => m.Name == appdataNutstoreDir);
 
-            // 构造调用NutstoreLib.Utils.DirectoryUtils.get_NUTSTORE_INSTALL_DIR的指令
-            var getInstallDirMethod = appdataDirMethod.Module.ImportReference(
-                appdataDirMethod.DeclaringType.Module.Types
-                    .First(t => t.Name == "DirectoryUtils" && t.Namespace == "NutstoreLib.Utils")
-                    .Methods.First(m => m.Name == "get_NUTSTORE_INSTALL_DIR")
-            );
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Call, getInstallDirMethod));
+                // 清除当前指令
+                appdataDirMethod.Body.Instructions.Clear();
 
-            // 构造调用System.IO.DirectoryInfo构造函数的指令
-            var directoryInfoConstructor = appdataDirMethod.Module.ImportReference(
-                typeof(System.IO.DirectoryInfo).GetConstructor(new[] { typeof(string) })
-            );
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Newobj, directoryInfoConstructor));
+                // 获取IL处理器
+                var ilProcessor = appdataDirMethod.Body.GetILProcessor();
 
-            // 构造获取DirectoryInfo.Parent属性的调用指令
-            var getParentMethod = appdataDirMethod.Module.ImportReference(
-                typeof(System.IO.DirectoryInfo).GetProperty("Parent").GetGetMethod()
-            );
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Call, getParentMethod));
+                // 构造调用NutstoreLib.Utils.DirectoryUtils.get_NUTSTORE_INSTALL_DIR的指令
+                var getInstallDirMethod = appdataDirMethod.Module.ImportReference(
+                    appdataDirMethod.DeclaringType.Module.Types
+                        .First(t => t.Name == "DirectoryUtils" && t.Namespace == "NutstoreLib.Utils")
+                        .Methods.First(m => m.Name == "get_NUTSTORE_INSTALL_DIR")
+                );
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Call, getInstallDirMethod));
 
-            // 构造调用System.IO.FileSystemInfo.FullName属性的指令
-            var getFullNameMethod = appdataDirMethod.Module.ImportReference(
-                typeof(System.IO.FileSystemInfo).GetProperty("FullName").GetGetMethod()
-            );
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Callvirt, getFullNameMethod));
+                // 构造调用System.IO.DirectoryInfo构造函数的指令
+                var directoryInfoConstructor = appdataDirMethod.Module.ImportReference(
+                    typeof(System.IO.DirectoryInfo).GetConstructor(new[] { typeof(string) })
+                );
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Newobj, directoryInfoConstructor));
 
-            // 加载字符串"UserData"
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Ldstr, "UserData"));
+                // 构造获取DirectoryInfo.Parent属性的调用指令
+                var getParentMethod = appdataDirMethod.Module.ImportReference(
+                    typeof(System.IO.DirectoryInfo).GetProperty("Parent").GetGetMethod()
+                );
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Call, getParentMethod));
 
-            // 构造调用Pri.LongPath.Path.Combine的指令
-            var pathCombineMethod = appdataDirMethod.Module.ImportReference(
+                // 构造调用System.IO.FileSystemInfo.FullName属性的指令
+                var getFullNameMethod = appdataDirMethod.Module.ImportReference(
+                    typeof(System.IO.FileSystemInfo).GetProperty("FullName").GetGetMethod()
+                );
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Callvirt, getFullNameMethod));
+
+                // 加载字符串"UserData"
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Ldstr, "UserData"));
+
+                // 构造调用Pri.LongPath.Path.Combine的指令
+                var pathCombineMethod = appdataDirMethod.Module.ImportReference(
                     typeof(System.IO.Path).GetMethod("Combine", new[] { typeof(string), typeof(string) })
                 );
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Call, pathCombineMethod));
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Call, pathCombineMethod));
 
-          // 返回指令
-            ilProcessor.Append(ilProcessor.Create(OpCodes.Ret));
-            
-            // Write the modified assembly
-            assemblyDefinition.Write(nutstoreLib);
+                // 返回指令
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Ret));
+
+                // Write the modified assembly
+                assemblyDefinition.Write(nutstoreLibTarget);
+            }
+            File.Copy(nutstoreLibTarget,nutstoreLib,true);
+            File.Delete(nutstoreLibTarget);
         }
     }
 }
